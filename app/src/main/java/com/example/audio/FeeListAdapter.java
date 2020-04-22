01f83955699ffabe598468d47e948a6681bf4195
DC <dc240498@gmail.com>
2020-04-22 23:40:31 +0530

    22/04

--- app/src/main/java/com/example/audio/FeeListAdapter.java
+++ app/src/main/java/com/example/audio/FeeListAdapter.java
@@ -1,4 +1,48 @@
 package com.example.audio;
 
-public class FeeListAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class FeeListAdapter extends ArrayAdapter<FeePojo> {
+
+    Context context;
+    int resource;
+
+    public FeeListAdapter(@NonNull Context context, int resource) {
+        super(context, resource);
+        this.context = context;
+        this.resource = resource;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        if(convertView==null)
+            convertView = LayoutInflater.from(context).inflate(resource,parent,false);
+
+        FeePojo feePojo = getItem(position);
+
+        TextView fee = convertView.findViewById(R.id.fee_mny);
+        TextView time = convertView.findViewById(R.id.fee_time);
+        TextView remark = convertView.findViewById(R.id.fee_remark);
+
+
+
+
+        fee.setText(feePojo.getFee());
+        remark.setText(feePojo.getRemark());
+        time.setText(feePojo.getTime());
+
+
+
+        return convertView;
+    }
 }
